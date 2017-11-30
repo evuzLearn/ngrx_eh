00e440b60bc81e27c1016471eb83d6c9e9e2ade1
evuz <jesus_jgb_45@hotmail.com>
2017-11-30 15:07:11 +0100

    Handle Click Events with Subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/map';
@@ -6,13 +7,15 @@ import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-root',
   template: `
+    <button (click)="click$.next()">Update</button>
     <h1>{{ clock | async | date:'dd/MM/y hh:mm:ss' }}</h1>
   `,
 })
 export class AppComponent {
-  clock = Observable.interval(1000).map(() => new Date());
+  click$ = new Subject();
+  clock;
 
   constructor() {
-    this.clock.subscribe(console.log.bind(console));
+    this.clock = this.click$.map(() => new Date());
   }
 }
